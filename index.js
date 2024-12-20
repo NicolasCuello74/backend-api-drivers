952032e142a632b23af6a8c63cc12a2c2bb8d142
NicolasCuello74 <nicocuello7@gmail.com>
2024-12-20 10:57:02 -0300

    backend-fina6

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const server = require("./src/servers.js");
 const { conn } = require('./src/db.js');
 
-conn.sync({ force: false }).then(() => {
-server.listen(3001, () => {
-  console.log(`Server listening on port`);
-})
-}).catch(error => console.error(error))
+const PORT = process.env.PORT || 3001;
 
+conn.sync({ force: false }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}).catch(error => console.error(error));
